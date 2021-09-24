3ef6fcc80db05a79843084f93d8f939604a32cc5
Baxtiyor98 <ergashevb111@gmail.com>
2021-09-24 12:39:59 +0500

    Some

--- FooFun/settings.py
+++ FooFun/settings.py
@@ -123,12 +123,12 @@ import os
 STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, 'static'),
-# )
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"
+# ]
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(PROJECT_ROOT,'staticfiles')
 
