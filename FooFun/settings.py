2b88aadf4f62dd7bd2eb7b0db8c86b2ca51eba2b
baxtikdev <ergashevb111@gmail.com>
2023-11-12 17:04:50 +0500

    news

--- FooFun/settings.py
+++ FooFun/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$2ek6#qwyl6nrfu@yv73g(h&s3@qee2^$#-bakhxho0ayrno89
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['funnyfoodapp.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
